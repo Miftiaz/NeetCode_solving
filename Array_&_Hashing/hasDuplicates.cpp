14ed7d4d1cd2323c151dc9e80d28d5abe676f8b9
miftiaz <miftiazintisami01@gmail.com>
2026-02-06 12:08:25 +0600

    initial commit

---
+++ Array_&_Hashing/hasDuplicates.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set <int> arra;
+        for ( int x: nums){
+            if (arra.count(x)){
+                return true;
+            }
+            else{
+                arra.insert(x);
+            }
+        }
+        return false;
+    }
+};
