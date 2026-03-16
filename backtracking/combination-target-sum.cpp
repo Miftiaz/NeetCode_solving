cf8fc5ac8a4a3bd5b5756170288dfdac1d23648f
miftiaz <miftiazintisami01@gmail.com>
2026-03-16 22:10:40 +0600

    solved combination-target-sum in optimal way

--- backtracking/combination-target-sum.cpp
+++ backtracking/combination-target-sum.cpp
@@ -2,7 +2,19 @@
 
 /*
 
-Solved using backtracking
+Solved using backtracking 
+
+In every step:
+
+We'll add the current indexed value to the string or won't.
+
+- if we add, pass the same index and target = target - arr[i]
+- if we don't add it, then increment the index.
+
+- If the target < 0 that means we added more than needed. so, STOP!
+- If i is out of bound, STOP!
+
+Check all combinations.
 
 */
 
@@ -30,6 +42,43 @@ public:
 
         curr.pop_back();
         backtrack(arr, i+1, curr, target);
+
+    }
+};
+
+/*
+
+Optimal Backtracking.
+
+1. Sort the array. So that if we encounter a value that makes our sum greater than target, the later values will face the same thing. So, it ensures early stop.
+2. In every step, we go level wise. Add all combinations the adding the values in the array (including index j and more)
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> curr;
+        sort(nums.begin(), nums.end());
+        backtrack(nums, 0, curr, 0, target);
+        return res;    
+    }
+
+    void backtrack(vector<int>& arr, int i, vector<int>& curr, int sum, int target){
+        if(sum == target) {
+            res.push_back(curr);
+            return;
+        }
+
+        for(int j=i; j<arr.size(); j++) {
+            if(sum + arr[j] > target){
+                return;
+            }
+            curr.push_back(arr[j]);
+            backtrack(arr, j, curr, sum+arr[j], target);
+            curr.pop_back();
+        }
 
     }
 };
